8e9e1220238a6e1c986db9f176db3d33a9a0f8b3
Jimmyh091 <jaimecaballero164@gmail.com>
2025-08-09 12:59:07 +0200

    Nueva estructura

---
+++ basico/mayor-menor/ejercicio.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(){
+    
+    int num1;
+    int num2;
+    int num3;
+
+    std::cout << "Introduce el primer numero\n>";
+    std::cin >> num1;
+
+    std::cout << "Introduce el segnudo numero\n>";
+    std::cin >> num2;
+
+    std::cout << "Introduce el tercer numero\n>";
+    std::cin >> num3;
+
+    switch (num1)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
