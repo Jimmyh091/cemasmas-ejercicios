761e2b11dfc46253d0475185c04aa3dbaaf65b99
Jimmyh091 <jaimecaballero164@gmail.com>
2025-08-09 13:12:03 +0200

    Ejercicio mayor-menor

--- basico/mayor-menor/ejercicio.cpp
+++ basico/mayor-menor/ejercicio.cpp
@@ -15,13 +15,23 @@ int main(){
     std::cout << "Introduce el tercer numero\n>";
     std::cin >> num3;
 
-    switch (num1)
-    {
-    case constant expression:
-        /* code */
-        break;
+    bool mayornum1 = num1 > num2;
+    bool mayornum2 = num2 > num3;
+    bool mayornum3 = num3 > num1;
+
+    if (mayornum1) 
+        std::cout << "\nEl primer numero es mayor que el segundo";        
+    else
+        std::cout << "\nEl primer numero es menor que el segundo";
     
-    default:
-        break;
-    }
+
+    if (mayornum2) 
+        std::cout << ", el segundo numero es mayor que el tercero";    
+    else    
+        std::cout << ", el segundo numero es menor que el tercero";
+    
+    if (mayornum3) 
+        std::cout << " y el tercero es mayor que el primero.";
+    else
+        std::cout << " y el tercero es menor que el primero.";
 }
